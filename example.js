64e7bcd227c2dae27d9ee1ac39305092865c5ebf
roman-strilets <roman@beam-mw.com>
2021-06-17 13:01:36 +0300

    small refactoring

--- example.js
+++ example.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 
 const beam = require('./utils/beam_utils.js');
+const ethash_utils = require('./utils/ethash_utils.js');
 const Web3 = require('web3');
-const BN = Web3.utils.BN;
-const RLP = require('rlp');
-const keccak256 = require('keccak256');
-const keccak512 = require('js-sha3').keccak512;
-const Net = require('net');
 
 let web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_HTTP_PROVIDER));
 
@@ -24,76 +20,6 @@ async function waitTx(txId) {
     } while(true)
 }
 
-const changeEndianness = (string) => {
-    string = string.replace(/^0x/i, '');
-    const result = [];
-    let len = string.length - 2;
-    while (len >= 0) {
-      result.push(string.substr(len, 2));
-      len -= 2;
-    }
-    return result.join('');
-}
-
-function generateSeed(block) {
-    // look at https://github.com/pantos-io/ethrelay/blob/master/utils/utils.js
-    let encoded = RLP.encode([
-        block.parentHash,
-        block.sha3Uncles,
-        block.miner,
-        block.stateRoot,
-        block.transactionsRoot,
-        block.receiptsRoot,
-        block.logsBloom,
-        new BN(block.difficulty),
-        new BN(block.number),
-        block.gasLimit,
-        block.gasUsed,
-        block.timestamp,
-        block.extraData
-    ]);
-    let prePoWBlockHash = keccak256(encoded).toString('hex');
-    let tmp = prePoWBlockHash + changeEndianness(block.nonce);
-    
-    return keccak512(Buffer.from(tmp, 'hex'));
-}
-
-function requestProof(number, seed) {
-    return new Promise((resolve, reject) => {
-        let client = new Net.Socket();
-        let acc = '';
-        
-        client.connect(process.env.ETHASH_SERVICE_PORT, process.env.ETHASH_SERVICE_HOST, () => {
-            client.write(JSON.stringify(
-                {
-                    jsonrpc: '2.0',
-                    id: 123,
-                    method: 'get_proof',
-                    params: {
-                        "epoch": number,
-                        "seed": seed
-                    }
-                }) + '\n');
-        });
-
-        client.on('data', function(data) {
-            acc += data;
-
-            if (data.indexOf('\n') != -1) {
-                let res = JSON.parse(acc);
-                resolve([res['result']['proof'], res['result']['dataset_count']]);
-                client.destroy();
-            }
-        });
-
-        client.on('close', function() {
-            console.log('Connection closed');
-        });
-
-        client.on('error', reject);
-    });
-};
-
 (async () => {
     let pubkey = await beam.readPk();
     
@@ -106,11 +32,11 @@ function requestProof(number, seed) {
 
     console.log('block = ', block);
 
-    let seed = generateSeed(block);
+    let seed = ethash_utils.generateSeed(block);
 
     let epoch = Math.floor(block.number / 30000);
     //let seed2 = await beam.genearateSeed(block);
-    let [proof, datasetCount] = await requestProof(epoch, seed);
+    let [proof, datasetCount] = await ethash_utils.requestProof(epoch, seed);
 
     console.log('epoch = ', epoch);
     console.log('seed = ', seed);
---
+++ utils/ethash_utils.js
@@ -0,0 +1,76 @@
+const Web3 = require('web3');
+const BN = Web3.utils.BN;
+const RLP = require('rlp');
+const keccak256 = require('keccak256');
+const keccak512 = require('js-sha3').keccak512;
+const Net = require('net');
+
+const changeEndianness = (string) => {
+    string = string.replace(/^0x/i, '');
+    const result = [];
+    let len = string.length - 2;
+    while (len >= 0) {
+      result.push(string.substr(len, 2));
+      len -= 2;
+    }
+    return result.join('');
+}
+
+exports.generateSeed = (block) => {
+    // look at https://github.com/pantos-io/ethrelay/blob/master/utils/utils.js
+    let encoded = RLP.encode([
+        block.parentHash,
+        block.sha3Uncles,
+        block.miner,
+        block.stateRoot,
+        block.transactionsRoot,
+        block.receiptsRoot,
+        block.logsBloom,
+        new BN(block.difficulty),
+        new BN(block.number),
+        block.gasLimit,
+        block.gasUsed,
+        block.timestamp,
+        block.extraData
+    ]);
+    let prePoWBlockHash = keccak256(encoded).toString('hex');
+    let tmp = prePoWBlockHash + changeEndianness(block.nonce);
+    
+    return keccak512(Buffer.from(tmp, 'hex'));
+}
+
+exports.requestProof = (number, seed) => {
+    return new Promise((resolve, reject) => {
+        let client = new Net.Socket();
+        let acc = '';
+        
+        client.connect(process.env.ETHASH_SERVICE_PORT, process.env.ETHASH_SERVICE_HOST, () => {
+            client.write(JSON.stringify(
+                {
+                    jsonrpc: '2.0',
+                    id: 123,
+                    method: 'get_proof',
+                    params: {
+                        "epoch": number,
+                        "seed": seed
+                    }
+                }) + '\n');
+        });
+
+        client.on('data', function(data) {
+            acc += data;
+
+            if (data.indexOf('\n') != -1) {
+                let res = JSON.parse(acc);
+                resolve([res['result']['proof'], res['result']['dataset_count']]);
+                client.destroy();
+            }
+        });
+
+        client.on('close', function() {
+            console.log('Connection closed');
+        });
+
+        client.on('error', reject);
+    });
+}
