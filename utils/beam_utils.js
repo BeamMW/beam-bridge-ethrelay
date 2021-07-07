41cf13994d13956a0a6e84580236260dcac8b6e3
Alex Starun <alex.starun@beam-mw.com>
2021-07-07 17:20:50 +0200

    Fixed getUserPubkey

--- utils/beam_utils.js
+++ utils/beam_utils.js
@@ -237,11 +237,11 @@ exports.bridgePushRemote = (msgId, contractReceiver, contractSender, msgBody, bl
 exports.getUserPubkey = () => {
     return baseFunction(
         process.env.BEAM_SHADERS_PATH + '/mirrortoken/app.wasm',
-        'role=user,action=view_addr, cid=' + process.env.BEAM_PIPE_USER_CID,
+        'role=user,action=get_pk, cid=' + process.env.BEAM_PIPE_USER_CID,
         (data) => {
             let res = JSON.parse(data);
             let output = JSON.parse(res['result']['output']);
-            return output['addr'];
+            return output['pk'];
         }
     );
 };
