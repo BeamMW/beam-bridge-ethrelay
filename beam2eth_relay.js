115ce59cd9da04c5eaeccced5ffec454ccf53ca8
roman-strilets <roman@beam-mw.com>
2021-07-28 12:44:34 +0300

    store last processing msgID or block height

--- beam2eth_relay.js
+++ beam2eth_relay.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+let fs = require('fs');
+
 function currentTime() {
     return "[" + (new Date()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'}) + "] ";
 }
@@ -7,18 +9,38 @@ function currentTime() {
 const beam = require('./utils/beam_utils.js');
 const eth = require('./utils/eth_utils.js');
 const {program} = require('commander');
+const SETTINGS_FILE = './beam2eth_settings.json';
 
-program.option('-m, --msgId <number>', 'start message id', 23);
+function saveSettings(value) {
+    try {
+        fs.writeFileSync(SETTINGS_FILE, JSON.stringify({
+            'startMsgId': value
+        }));
+    } catch (e) {}
+}
+
+program.option('-m, --msgId <number>', 'start message id');
 
 program.parse(process.argv);
 
 const options = program.opts();
-let startMsgId = options.msgId;
+let startMsgId = 1;
+
+if (options.msgId !== undefined) {
+    startMsgId = options.msgId;
+    saveSettings(startMsgId);
+} else {
+    try {
+        let data = fs.readFileSync(SETTINGS_FILE);
+        let obj = JSON.parse(data);
+        startMsgId = obj['startMsgId'];
+    } catch (e) { }
+}
 
 async function monitorBridge() {
     let count = await beam.getLocalMsgCount();
     
-    for (; startMsgId <= count; startMsgId++) {
+    while (startMsgId <= count) {
         let localMsg = await beam.getLocalMsg(startMsgId);
         console.log(currentTime(), "Processing of a new message has started. Message ID - ", startMsgId);
         //console.log('msg: ', localMsg);
@@ -33,6 +55,7 @@ async function monitorBridge() {
 
         await eth.validateRemoteMessage(startMsgId, msgProof['proof'], blockDetails);
         console.log(currentTime(), "The message was successfully transferred to the Ethereum. Message ID - ", startMsgId);
+        saveSettings(++startMsgId);
     }
 
     setTimeout(monitorBridge, 15 * 1000);
--- eth2beam_relay.js
+++ eth2beam_relay.js
@@ -5,8 +5,10 @@ const ethash_utils = require('./utils/ethash_utils.js');
 const eth_utils = require('./utils/eth_utils.js');
 const Web3 = require('web3');
 const RLP = require('rlp');
+const fs = require('fs');
 
 const PipeContract = require('./utils/Pipe.json');
+const SETTINGS_FILE = './eth2beam_settings.json';
 
 let web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ETH_WEBSOCKET_PROVIDER));
 
@@ -18,6 +20,14 @@ const pipeContract = new web3.eth.Contract(
 // TODO roman.strilets 
 function currentTime() {
     return "[" + (new Date()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'}) + "] ";
+}
+
+function saveSettings(value) {
+    try {
+        fs.writeFileSync(SETTINGS_FILE, JSON.stringify({
+            'startBlock': value
+        }));
+    } catch (e) {}
 }
 
 async function processEvent(event) {
@@ -46,19 +56,32 @@ async function processEvent(event) {
 
     await beam.waitTx(pushRemoteTxID);
     console.log(currentTime(), "The message was successfully transferred to the Beam. Message ID - ", event["returnValues"]["msgId"]);
+    saveSettings(block.number + 1);
 }
 
 const {program} = require('commander');
 
-program.option('-b, --startBlock <number>', 'start block', 0);
+program.option('-b, --startBlock <number>', 'start block');
 
 program.parse(process.argv);
 
 const options = program.opts();
+let startBlock = 0;
+
+if (options.startBlock !== undefined) {
+    startBlock = options.startBlock;
+    saveSettings(startBlock);
+} else {
+    try {
+        let data = fs.readFileSync(SETTINGS_FILE);
+        let obj = JSON.parse(data);
+        startBlock = obj['startBlock'];
+    } catch (e) { }
+}
 
 // subscribe to Pipe.NewLocalMessage
 pipeContract.events.NewLocalMessage({
-    fromBlock: options.startBlock
+    fromBlock: startBlock
 }, function(error, event) { /*console.log(event);*/ })
 .on("connected", function(subscriptionId) {
     console.log('subscription id: ', subscriptionId);
