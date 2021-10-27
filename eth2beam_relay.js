98e2bc5b56e00387db8c8be36d5ce6621b269259
Alex Starun <alex.starun@beam-mw.com>
2021-10-27 20:45:41 +0200

    Added confirmations check

--- eth2beam_relay.js
+++ eth2beam_relay.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const beam = require('./utils/beam_utils.js');
 const Web3 = require('web3');
 const fs = require('fs');
+const {program} = require('commander');
 
 const PipeContract = require('./utils/Pipe.json');
 const SETTINGS_FILE = './eth2beam_settings.json';
@@ -24,7 +25,55 @@ function saveSettings(value) {
         fs.writeFileSync(SETTINGS_FILE, JSON.stringify({
             'startBlock': value
         }));
-    } catch (e) {}
+    } catch (error) {
+        console.log(currentTime(), "Faield to save settings - ", error);
+    }
+}
+
+program.option('-b, --startBlock <number>', 'start block');
+program.parse(process.argv);
+
+const options = program.opts();
+let startBlock = 0;
+
+if (options.startBlock !== undefined) {
+    startBlock = options.startBlock;
+    saveSettings(startBlock);
+} else {
+    try {
+        let data = fs.readFileSync(SETTINGS_FILE);
+        let obj = JSON.parse(data);
+        startBlock = obj['startBlock'];
+    } catch (error) {
+        console.log(currentTime(), "Failed to load settings - ", error);
+    }
+}
+
+// TOOD: switch to DB?
+let events = new Map();
+
+function addEvent(event) {
+    if (!events.has(event['blockNumber'])) {
+        events.set(event['blockNumber'], new Map());
+    }
+    events.get(event['blockNumber']).set(event['transactionHash'], event);
+}
+
+function removeEvent(event) {
+    if (events.has(event['blockNumber'])) {
+        events.get(event['blockNumber']).delete(event['transactionHash']);
+    }
+}
+
+async function onGotNewBlock(blockHeader) {
+    const minBlockNumber = blockHeader['number'] - process.env.ETH_MIN_CONFRIMATIONS;
+    for (const blockNumber of events.keys()) {
+        if (blockNumber <= minBlockNumber) {
+            for (const event of events.get(blockNumber).values()) {
+                processEvent(event);
+            }
+        }
+    }
 }
 
 async function processEvent(event) {
@@ -42,41 +91,21 @@ async function processEvent(event) {
     saveSettings(event['blockNumber'] + 1);
 }
 
-const {program} = require('commander');
-
-program.option('-b, --startBlock <number>', 'start block');
-
-program.parse(process.argv);
-
-const options = program.opts();
-let startBlock = 0;
-
-if (options.startBlock !== undefined) {
-    startBlock = options.startBlock;
-    saveSettings(startBlock);
-} else {
-    try {
-        let data = fs.readFileSync(SETTINGS_FILE);
-        let obj = JSON.parse(data);
-        startBlock = obj['startBlock'];
-    } catch (e) { }
-}
-
 // subscribe to Pipe.NewLocalMessage
 pipeContract.events.NewLocalMessage({
     fromBlock: startBlock
 }, function(error, event) { /*console.log(event);*/ })
 .on("connected", function(subscriptionId) {
-    console.log('subscription id: ', subscriptionId);
+    console.log(currentTime(), 'Pipe.NewLocalMessage: successfully subcribed, subscription id: ', subscriptionId);
 })
 .on('data', function(event) {
-    //console.log("New event: ", event);
-    // TODO: wait enough confirmations?
-    processEvent(event);
+    console.log(currentTime(), "Got new event: ", event);
+    addEvent(event);
 })
 .on('changed', function(event) {
     // remove event from local database
-    console.log("event changed!");
+    console.log(currentTime(), "Event changed! ", event);
+    removeEvent(event);
 })
 .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
     console.log("Error: ", error);
@@ -84,3 +113,19 @@ pipeContract.events.NewLocalMessage({
         console.log("receipt: ", receipt);
     }
 });
+
+let newBlockSubscription = web3.eth.subscribe('newBlockHeaders')
+.on("connected", function(subscriptionId){
+    console.log(currentTime(), 'newBlockHeaders: successfully subcribed, subscription id - ', subscriptionId);
+})
+.on("data", function(blockHeader){
+    onGotNewBlock(blockHeader);
+})
+.on("error", console.error);
+
+// // unsubscribes the newBlockSubscription
+// newBlockSubscription.unsubscribe(function(error, success){
+//     if (success) {
+//         console.log('Successfully unsubscribed!');
+//     }
+// });
