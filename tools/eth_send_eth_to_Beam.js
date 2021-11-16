521e28bb7a5f96535d5e2624583787232de906a4
roman-strilets <roman@beam-mw.com>
2021-11-16 12:13:43 +0300

    added missed file

---
+++ tools/eth_send_eth_to_Beam.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const eth_utils = require('./../utils/eth_utils.js');
+const Web3 = require('web3');
+const PipeContract = require('./../utils/EthPipeContractABI.js');
+
+let web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_HTTP_PROVIDER));
+
+const pipeContract = new web3.eth.Contract(
+    PipeContract.abi,
+    process.env.ETH_PIPE_CONTRACT_ADDRESS
+);
+
+const {program} = require('commander');
+
+program.option('-a, --amount <number>', 'amount of 10 Gwei to send', 5000000000);
+program.option('-f, --fee <number>', 'relayer fee of 10 Gwei', 100000000);
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+lockEthereum = async (amount, pubkey, relayerFee) => {
+    console.log('provider: ', process.env.ETH_HTTP_PROVIDER)
+    console.log('sender: ', process.env.ETH_SENDER)
+    const lockTx = pipeContract.methods.sendFunds(amount, relayerFee, pubkey);
+    // convert to wei
+    const total = BigInt(amount + relayerFee) * 10 * 1000000000;
+
+    let lockTxReceipt = await eth_utils.requestToContract(
+        process.env.ETH_SENDER,
+        process.env.ETH_PIPE_CONTRACT_ADDRESS,
+        process.env.ETH_SENDER_PRIVATE_KEY,
+        // TODO roman.strilets change this parameter
+        process.env.PUSH_REMOTE_GAS_LIMIT,
+        lockTx.encodeABI(),
+        total);
+
+    //console.log(lockTxReceipt);
+    return lockTxReceipt;
+}
+
+(async () => {
+    console.log("Calling 'sendFunds' of Pipe contract:");
+    const amount = options.amount;
+    const relayerFee = options.fee;
+
+    // lock Ethereums' on Ethereum chain
+    let receipt = await lockEthereum(amount, process.env.BEAM_PUBLIC_KEY, relayerFee);
+
+    //console.log("TX receipt: ", receipt);
+    console.log("'sendFunds' is finished.")
+})();
