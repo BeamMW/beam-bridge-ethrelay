32f4ce16b47ae079cba2b865a937bb004df408d9
Alex Starun <alex.starun@beam-mw.com>
2021-12-03 15:17:11 +0100

    WIP: added validation of the relayerFee

--- beam2eth_relay.js
+++ beam2eth_relay.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const beam = require('./utils/beam_utils.js');
 const eth = require('./utils/eth_utils.js');
 const {program} = require('commander');
+const https = require('https');
 
 let fs = require('fs');
 let msgId = 1;
@@ -31,8 +32,50 @@ async function requestHeight() {
     return 0;
 }
 
-function isValidRelayerFee(relayerFee) {
-    return true;
+function baseGetRequest(url, processResult) {
+    return new Promise((resolve, reject) => {
+        let accumulated = '';
+
+        let callback = (response) => {
+            response.on('data', (chunk) => {
+                accumulated += chunk;
+            });
+
+            response.on('end', () => {
+                resolve(processResult(accumulated));
+            });
+
+            response.on('error', reject);
+        }
+
+        https.get(url, callback).on('error', reject);
+    });
+}
+
+async function getCurrencyRate(rateId) {
+    const url = `${process.env.COINGECKO_CURRENCY_RATE_API_URL}?ids=${rateId}&vs_currencies=usd`;
+    return baseGetRequest(url, JSON.parse);
+}
+
+async function getGasPrice() {
+    return baseGetRequest(process.env.GAS_PRICE_API_URL, JSON.parse);
+}
+
+async function calcCurrentRelayerFee(rateId) {
+    const RELAY_COSTS_IN_GAS = 120000;
+    const ETH_RATE_ID = 'ethereum';
+
+    const gasPrice = await getGasPrice();
+    const ethRate = await getCurrencyRate(ETH_RATE_ID)
+    const relayCosts = RELAY_COSTS_IN_GAS * parseFloat(gasPrice['FastGasPrice']) * parseFloat(ethRate[ETH_RATE_ID]['usd']) / Math.pow(10, 9);
+    const currRate = await getCurrencyRate(rateId);
+
+    return relayCosts / parseFloat(currRate[rateId]['usd']);
+}
+
+async function isValidRelayerFee(relayerFee) {
+    const estimatedRelayerFee = await calcCurrentRelayerFee(process.env.COINGECKO_CURRENCY_RATE_ID);
+    return relayerFee >= estimatedRelayerFee;
 }
 
 async function monitorBridge() {
@@ -68,7 +111,7 @@ async function monitorBridge() {
                     relayerFee = relayerFee.slice(0, -diff);
                 }
 
-                if (isValidRelayerFee(relayerFee)) {
+                if (await isValidRelayerFee(relayerFee)) {
                     console.log(currentTime(), "Processing of a new message has started. Message ID - ", msgId);
 
                     await eth.processRemoteMessage(msgId, amount, localMsg['receiver'], relayerFee);
