c0a25ffa875a924459270becfa7533e749271e48
roman-strilets <roman@beam-mw.com>
2021-12-13 13:49:06 +0300

    revert EthPipeContractABI.js

--- utils/EthPipeContractABI.js
+++ utils/EthPipeContractABI.js
@@ -1,3 +1,4 @@
-import PipeContract from "${process.env.ETH_PIPE_CONTRACT_ABI}";
+const PipeContract = require(process.env.ETH_PIPE_CONTRACT_ABI);
+//import PipeContract from process.env.ETH_PIPE_CONTRACT_ABI
 
 module.exports = PipeContract;
