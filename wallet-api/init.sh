b975d8cbb4b41662e4571f76d10a1c7bec5809ee
Alex Starun <alex.starun@beam-mw.com>
2022-05-24 08:44:51 +0200

    Added wallet-api container with updated binaries to 7.1.12740

---
+++ wallet-api/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+trap '' SIGINT
+if [ ! -f "/home/beam/data/wallet.db" ]; then
+  /home/beam/wallet/beam-wallet $BEAM_CUSTOM_NETWORK_ARGS --pass=$WALLET_PASS --wallet_path=/home/beam/data/wallet.db restore --seed_phrase=$SEED_PHRASE
+  echo
+  echo 'wallet.db restore process done. Ready for start up.'
+  echo
+else
+  echo
+  echo 'wallet.db is already restored! Ready for start up.'
+  echo
+fi
+
