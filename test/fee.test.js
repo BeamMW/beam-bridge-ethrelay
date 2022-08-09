e6dee338bfe901a8c9f98fc70d7ab242153092a0
roman-strilets <roman@beam-mw.com>
2022-08-09 17:53:41 +0300

    added a few tests

---
+++ test/fee.test.js
@@ -0,0 +1,307 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+import http from "http";
+import {calcCurrentRelayerFee} from "./../utils/eth_fee.js"
+import assert from "assert";
+
+describe("", () => {
+    before(() => {
+        process.env.COINGECKO_CURRENCY_RATE_API_URL = "http://127.0.0.1:9998";
+        process.env.GAS_PRICE_API_URL = "http://127.0.0.1:9999";
+    });
+
+    it("normal case", async() => {
+        const coingecko = http.createServer((req, res) => {
+            res.writeHead(200);
+            if (req.url.includes('ethereum')) {
+                res.end(`{ "ethereum": { "usd": 1715.93 } }`);
+            } else {
+                res.end(`{ "tether": { "usd": 1.002 } }`);
+            }
+        });
+
+        coingecko.listen(9998);
+
+        const etherscan = http.createServer((req, res) => {
+            res.writeHead(200);
+            res.end(`{
+                "FastGasPrice": 37
+              }`);
+        });
+
+        etherscan.listen(9999);
+
+        const fee = await calcCurrentRelayerFee("tether", false);
+        assert.ok(fee > 7 && fee < 8);
+
+        coingecko.close();
+        etherscan.close();
+    });
+
+    it("divide by zero", async() => {
+        const coingecko = http.createServer((req, res) => {
+            res.writeHead(200);
+            if (req.url.includes('ethereum')) {
+                res.end(`{ "ethereum": { "usd": 1715.93 } }`);
+            } else {
+                res.end(`{ "tether": { "usd": 0 } }`);
+            }
+        });
+
+        coingecko.listen(9998);
+
+        const etherscan = http.createServer((req, res) => {
+            res.writeHead(200);
+            res.end(`{
+                "FastGasPrice": 37
+              }`);
+        });
+
+        etherscan.listen(9999);
+
+        const fee = await calcCurrentRelayerFee("tether", false);
+
+        console.log(fee);
+        
+        coingecko.close();
+        etherscan.close();
+    });
+
+    // it("empty response of coingecko", async() => {
+    //     const coingecko = http.createServer((req, res) => {
+    //         res.writeHead(404);
+    //         res.end("");
+    //     });
+
+    //     coingecko.listen(9998);
+
+    //     const etherscan = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         res.end(`{
+    //             "FastGasPrice": 37
+    //           }`);
+    //     });
+
+    //     etherscan.listen(9999);
+
+    //     const fee = await calcCurrentRelayerFee("tether", false);
+
+    //     coingecko.close();
+    //     etherscan.close();
+    // });
+
+    // it("empty response of etherscan", async() => {
+    //     const coingecko = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         if (req.url.includes('ethereum')) {
+    //             res.end(`{ "ethereum": { "usd": 1715.93 } }`);
+    //         } else {
+    //             res.end(`{ "tether": { "usd": 1.002 } }`);
+    //         }
+    //     });
+
+    //     coingecko.listen(9998);
+
+    //     const etherscan = http.createServer((req, res) => {
+    //         res.writeHead(404);
+    //         res.end("");
+    //     });
+
+    //     etherscan.listen(9999);
+
+    //     const fee = await calcCurrentRelayerFee("tether", false);
+
+    //     coingecko.close();
+    //     etherscan.close();
+    // });
+
+    // it("wrong ethereum rate", async() => {
+    //     const coingecko = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         if (req.url.includes('ethereum')) {
+    //             res.end(`{ "ethereum": { "usd": "test" } }`);
+    //         } else {
+    //             res.end(`{ "tether": { "usd": 1.002 } }`);
+    //         }
+    //     });
+
+    //     coingecko.listen(9998);
+
+    //     const etherscan = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         res.end(`{
+    //             "FastGasPrice": 37
+    //           }`);
+    //     });
+
+    //     etherscan.listen(9999);
+
+    //     const fee = await calcCurrentRelayerFee("tether", false);
+
+    //     const expectedMinimumFee = Math.trunc(
+    //         Math.pow(10, process.env.ETH_SIDE_DECIMALS) * fee
+    //     );
+
+    //     console.log(fee);
+    //     console.log(expectedMinimumFee);
+    //     const t = BigInt(expectedMinimumFee);
+        
+    //     coingecko.close();
+    //     etherscan.close();
+    // });
+
+    // it("wrong tether rate", async() => {
+    //     const coingecko = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         if (req.url.includes('ethereum')) {
+    //             res.end(`{ "ethereum": { "usd": 1715.93 } }`);
+    //         } else {
+    //             res.end(`{ "tether": { "usd": "test" } }`);
+    //         }
+    //     });
+
+    //     coingecko.listen(9998);
+
+    //     const etherscan = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         res.end(`{
+    //             "FastGasPrice": 37
+    //           }`);
+    //     });
+
+    //     etherscan.listen(9999);
+
+    //     const fee = await calcCurrentRelayerFee("tether", false);
+
+    //     const expectedMinimumFee = Math.trunc(
+    //         Math.pow(10, process.env.ETH_SIDE_DECIMALS) * fee
+    //     );
+
+    //     console.log(fee);
+    //     console.log(expectedMinimumFee);
+    //     const t = BigInt(expectedMinimumFee);
+        
+    //     coingecko.close();
+    //     etherscan.close();
+    // });
+
+    // it("wrong etherscan gas price", async() => {
+    //     const coingecko = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         if (req.url.includes('ethereum')) {
+    //             res.end(`{ "ethereum": { "usd": 1715.93 } }`);
+    //         } else {
+    //             res.end(`{ "tether": { "usd": 1.002 } }`);
+    //         }
+    //     });
+
+    //     coingecko.listen(9998);
+
+    //     const etherscan = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         res.end(`{
+    //             "FastGasPrice": "test"
+    //           }`);
+    //     });
+
+    //     etherscan.listen(9999);
+
+    //     const fee = await calcCurrentRelayerFee("tether", false);
+
+    //     const expectedMinimumFee = Math.trunc(
+    //         Math.pow(10, process.env.ETH_SIDE_DECIMALS) * fee
+    //     );
+
+    //     console.log(fee);
+    //     console.log(expectedMinimumFee);
+    //     const t = BigInt(expectedMinimumFee);
+        
+    //     coingecko.close();
+    //     etherscan.close();
+    // });
+
+    it("other currency", async() => {
+        const coingecko = http.createServer((req, res) => {
+            res.writeHead(200);
+            if (req.url.includes('ethereum')) {
+                res.end(`{ "btc": { "usd": 1715.93 } }`);
+            } else {
+                res.end(`{ "tether": { "usd": 1.002 } }`);
+            }
+        });
+
+        coingecko.listen(9998);
+
+        const etherscan = http.createServer((req, res) => {
+            res.writeHead(200);
+            res.end(`{
+                "FastGasPrice": 37
+              }`);
+        });
+
+        etherscan.listen(9999);
+
+        await assert.rejects(calcCurrentRelayerFee("tether", false), TypeError);
+        
+        coingecko.close();
+        etherscan.close();
+    });
+
+    it("other currency 2", async() => {
+        const coingecko = http.createServer((req, res) => {
+            res.writeHead(200);
+            if (req.url.includes('ethereum')) {
+                res.end(`{ "ethereum": { "usd": 1715.93 } }`);
+            } else {
+                res.end(`{ "btc": { "usd": 1.002 } }`);
+            }
+        });
+
+        coingecko.listen(9998);
+
+        const etherscan = http.createServer((req, res) => {
+            res.writeHead(200);
+            res.end(`{
+                "FastGasPrice": 37
+              }`);
+        });
+
+        etherscan.listen(9999);
+
+        await assert.rejects(calcCurrentRelayerFee("tether", false), TypeError);
+        
+        coingecko.close();
+        etherscan.close();
+    });
+
+    // it("'FastGasPrice' is absent", async() => {
+    //     const coingecko = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         if (req.url.includes('ethereum')) {
+    //             res.end(`{ "ethereum": { "usd": 1715.93 } }`);
+    //         } else {
+    //             res.end(`{ "tether": { "usd": 1.002 } }`);
+    //         }
+    //     });
+
+    //     coingecko.listen(9998);
+
+    //     const etherscan = http.createServer((req, res) => {
+    //         res.writeHead(200);
+    //         res.end(`{
+    //             "test": 37
+    //           }`);
+    //     });
+
+    //     etherscan.listen(9999);
+    //     const fee = await calcCurrentRelayerFee("tether", false);
+
+    //     console.log('fee= ', fee);
+
+    //     //await assert.rejects(calcCurrentRelayerFee("tether", false), TypeError);
+        
+    //     coingecko.close();
+    //     etherscan.close();
+    // });
+});
--- utils/eth_fee.js
+++ utils/eth_fee.js
@@ -1,11 +1,11 @@
-
 import dotenv from "dotenv";
 
 dotenv.config();
 
+import http from "http";
 import https from "https";
 
-function baseGetRequest(url, processResult) {
+function baseGetRequest(url, processResult, useHttps = true) {
     return new Promise((resolve, reject) => {
         let accumulated = "";
 
@@ -22,31 +22,35 @@ function baseGetRequest(url, processResult) {
             response.on("error", reject);
         };
 
-        https.get(url, callback).on("error", reject);
+        if (useHttps) {
+            https.get(url, callback).on("error", reject);
+        } else {
+            http.get(url, callback).on("error", reject);
+        }
     });
 }
 
-async function getCurrencyRate(rateId) {
+async function getCurrencyRate(rateId, useHttps = true) {
     const url = `${process.env.COINGECKO_CURRENCY_RATE_API_URL}?ids=${rateId}&vs_currencies=usd`;
-    return baseGetRequest(url, JSON.parse);
+    return baseGetRequest(url, JSON.parse, useHttps);
 }
 
-async function getGasPrice() {
-    return baseGetRequest(process.env.GAS_PRICE_API_URL, JSON.parse);
+async function getGasPrice(useHttps = true) {
+    return baseGetRequest(process.env.GAS_PRICE_API_URL, JSON.parse, useHttps);
 }
 
-async function calcCurrentRelayerFee(rateId) {
+async function calcCurrentRelayerFee(rateId, useHttps = true) {
     const RELAY_COSTS_IN_GAS = 120000;
     const ETH_RATE_ID = "ethereum";
 
-    const gasPrice = await getGasPrice();
-    const ethRate = await getCurrencyRate(ETH_RATE_ID);
+    const gasPrice = await getGasPrice(useHttps);
+    const ethRate = await getCurrencyRate(ETH_RATE_ID, useHttps);
     const relayCosts =
         (RELAY_COSTS_IN_GAS *
             parseFloat(gasPrice["FastGasPrice"]) *
             parseFloat(ethRate[ETH_RATE_ID]["usd"])) /
         Math.pow(10, 9);
-    const currRate = await getCurrencyRate(rateId);
+    const currRate = await getCurrencyRate(rateId, useHttps);
 
     return relayCosts / parseFloat(currRate[rateId]["usd"]);
 }
